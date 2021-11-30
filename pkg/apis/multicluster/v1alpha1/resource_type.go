2b43629b0f9445391619e6c6779cc5c973a17946
chwetion <chwetion@foxmail.com>
2021-11-30 15:46:03 +0800

    add resource manage cr

---
+++ pkg/apis/multicluster/v1alpha1/resource_type.go
@@ -0,0 +1,36 @@
+package v1alpha1
+
+import (
+	"harmonycloud.cn/multi-cluster-manager/pkg/apis/multicluster/common"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type MultiClusterResource struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MultiClusterResourceSpec   `json:"spec,omitempty"`
+	Status MultiClusterResourceStatus `json:"status,omitempty"`
+}
+
+type MultiClusterResourceSpec struct {
+	Resource      *runtime.Unstructured `json:"resource,omitempty"`
+	ReplicasField string                `json:"replicasFiled,omitempty"`
+	Workload      bool                  `json:"workload,omitempty"`
+}
+
+type MultiClusterResourceStatus struct {
+	ClusterStatus []common.MultiClusterResourceClusterStatus `json:"clusters,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type MultiClusterResourceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []MultiClusterResource `json:"items"`
+}
